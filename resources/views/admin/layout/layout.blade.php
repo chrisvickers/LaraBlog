63dcdf61ced5778fa127aca71ea2acf978fa7d69
Chris Vickers <chrisvickers01@gmail.com>
2015-03-14 20:38:12 +0000

    amended header

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -2,18 +2,15 @@
 @include('admin.layout.nav')
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
-        <div class="breadcrumb">
-            <div class="pull-right">
-                @yield('headerButton')
+        <div class="breadcrumb row">
+            <div class="pull-left">
+                <div class="text-left">
+                    <h3>@yield('pageTitle')</h3>
+                </div>
             </div>
+            <div class="pull-right">@yield('headerButton')</div>
         </div>
 
-    </div><!--/.row-->
-
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">@yield('pageTitle')</h1>
-        </div>
     </div><!--/.row-->
     @yield('content')
 </div>	<!--/.main-->
